beb76c5d402fd693935b392f97b1db90d2058601
Lucas Cordeiro <lucas.cordeiro3030@gmail.com>
2024-07-19 22:23:08 -0300

    Ajustes iniciais do front e criação de mock de dados para testes antes do uso do banco de dados

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,18 @@ from flask import Blueprint, render_template, redirect, url_for, request
 
 main = Blueprint('main', __name__)
 
+
+# Simulação de função para obter empréstimos. Substituir pelo acesso ao banco de dados.
+def get_loans():
+    return [
+        {'id': 1, 'book': 'Livro A', 'user': 'User 1', 'due_date': '2024-08-01'},
+        {'id': 2, 'book': 'Livro B', 'user': 'User 2', 'due_date': '2024-08-15'},
+    ]
+
+
 @main.route('/')
 def index():
     return render_template('index.html')
-
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,11 +41,10 @@ def book_page():
 
 @main.route('/loan-page', methods=['GET', 'POST'])
 def loan_page():
+    loans = get_loans()  # Obtem a lista de empréstimos
     if request.method == "POST":
-
         return redirect(url_for('main.index'))
-
-    return render_template('loan_page.html')
+    return render_template('loan_page.html', loans=loans)
 
 @main.route('/deadline-page', methods=['GET', 'POST'])
 def deadline_page():
